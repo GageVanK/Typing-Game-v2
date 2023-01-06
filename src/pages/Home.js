fe1968ae46ef27ccc60dba8d009439725ae7a071
GageVanK <gagevank@gmail.com>
2023-01-06 00:59:04 -0500

    adding score/time to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,13 @@
 import { Button, Center } from '@mantine/core';
 
 
-
-  
-  
-
 //Adding Home Page
 export default function Home() {
   return(
     <div>
+    <p>Score:</p>
+    <p>Time:</p>
+
     
     <Center>
     <Button display="block" ta="center" variant="outline" radius="md" size="md">
